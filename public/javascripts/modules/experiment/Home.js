8b700793fa2a3aeaace228b653f8c2e7b8331746
Hailong Du <hailong.du8889@gmail.com>
2018-02-02 21:44:59 +0800

    flying icons

--- public/javascripts/modules/experiment/Home.js
+++ public/javascripts/modules/experiment/Home.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import PrettyPieChart from './PrettyPieChart'
+import {Icon} from 'antd'
 
 export default class Home extends React.Component { 
   render() {
     return (
       <div>
-        <PrettyPieChart/>
+        <div className='header-logo'>
+          <PrettyPieChart/>
+        </div>
+        {/* <div className='play-around'>
+
+        </div> */}
+        <div style={{width:200, height:200, position:'relative'}}>
+          <Icon type="car" style={{fontSize:20,paddingBottom:'100px'}} className='play-around-left' />
+          <Icon type="safety" style={{fontSize:20,paddingTop:180}} className='play-around-bottom'/>
+          <Icon type="tool" style={{fontSize:20,paddingTop:180,paddingLeft:180}} className='play-around-right'/>
+          <Icon type="like-o" style={{fontSize:20,paddingLeft:180}} className='play-around-top'/>
+          <Icon type="setting" style={{fontSize:20,marginTop:90,marginLeft:75,color:'#fa541c'}} className='center-icon'/>
+        </div>
+        {/* <div style={{width:200, height:200, position:'relative'}}>
+          <Icon className='play-around' type="shopping-cart"/>
+          <Icon className='play-around' type="compass"/>
+          <Icon className='play-around' type="smile-o"/>
+          <Icon className='play-around' type="rocket"/>
+          <Icon className='play-around' type="dashboard"/>
+        </div> */}
+        <div style={{width:200, height:200, position:'relative'}}>
+          <Icon type="shopping-cart" style={{fontSize:20,paddingBottom:'100px'}} className='play-around-left' />
+          <Icon type="smile-o" style={{fontSize:20,paddingTop:180}} className='play-around-bottom'/>
+          <Icon type="dashboard" style={{fontSize:20,paddingTop:180,paddingLeft:180}} className='play-around-right'/>
+          <Icon type="rocket" style={{fontSize:20,paddingLeft:180}} className='play-around-top'/>
+          <Icon type="compass" style={{fontSize:20,marginTop:90,marginLeft:75,color:'#fa541c'}} className='center-icon'/>
+        </div>
+        
+        
       </div>
     )
   }
--- public/javascripts/webpack_entry/main.js
+++ public/javascripts/webpack_entry/main.js
@@ -1,6 +1,7 @@
 import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css'
 import 'ant-design-pro/dist/ant-design-pro.css'
 import '../../stylesheets/style.css'
 import App from './App'
